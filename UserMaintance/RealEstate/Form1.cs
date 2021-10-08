f859d305324b9a85663dbaad71c869a81c8f5c52
Viktor Maric <viktor.maric@stud.uni-corvinus.hu>
2021-10-08 20:07:44 +0000

    Flat init.

--- UserMaintance/RealEstate/Form1.cs
+++ UserMaintance/RealEstate/Form1.cs
@@ -12,9 +12,24 @@ namespace RealEstate
 {
     public partial class Form1 : Form
     {
+
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+        }
+
     }
 }
