d8a0d1d8287a47bcc22513a4e2f06375b3d94122
Viktor Maric <viktor.maric@stud.uni-corvinus.hu>
2021-10-22 17:36:37 +0000

    Adatfeltőltés.

--- UserMaintance/VaR/VaR/Form1.cs
+++ UserMaintance/VaR/VaR/Form1.cs
@@ -12,9 +12,19 @@ namespace VaR
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
