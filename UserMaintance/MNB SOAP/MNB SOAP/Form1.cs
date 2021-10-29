0cadce5c1cd2bc38dd8f5ecaccc630b3cfc4006c
Viktor Maric <viktor.maric@stud.uni-corvinus.hu>
2021-10-29 18:55:12 +0000

    Add rateDate

--- UserMaintance/MNB SOAP/MNB SOAP/Form1.cs
+++ UserMaintance/MNB SOAP/MNB SOAP/Form1.cs
@@ -1,4 +1,5 @@
-﻿using MNB_SOAP.MnbServiceReference;
+﻿using MNB_SOAP.Entities;
+using MNB_SOAP.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,30 +9,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace MNB_SOAP
 {
     public partial class Form1 : Form
     {
+        BindingList<RateDate> Rates = new BindingList<RateDate>();
         public Form1()
         {
             InitializeComponent();
-            var mnbService = new MNBArfolyamServiceSoapClient();
 
-            var request = new GetExchangeRatesRequestBody()
-            {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
-            };
-
-            var response = mnbService.GetExchangeRates(request);
-            var result = response.GetExchangeRatesResult;
+            RefreshData();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
+        public void RefreshData()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+
+                chartRateData.DataSource = Rates;
+
+                var series = chartRateData.Series[0];
+                series.ChartType = SeriesChartType.Line;
+                series.XValueMember = "Date";
+                series.YValueMembers = "Value";
+                series.BorderWidth = 2;
+
+                var legend = chartRateData.Legends[0];
+                legend.Enabled = false;
+
+                var chartArea = chartRateData.ChartAreas[0];
+                chartArea.AxisX.MajorGrid.Enabled = false;
+                chartArea.AxisY.MajorGrid.Enabled = false;
+                chartArea.AxisY.IsStartedFromZero = false;
+            }
+        }
+
     }
 }
