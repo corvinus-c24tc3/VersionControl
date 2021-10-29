4575120184edc5ef3dcd962288cc12c589511495
Viktor Maric <viktor.maric@stud.uni-corvinus.hu>
2021-10-29 19:00:28 +0000

    Filters added.

--- UserMaintance/MNB SOAP/MNB SOAP/Form1.cs
+++ UserMaintance/MNB SOAP/MNB SOAP/Form1.cs
@@ -31,12 +31,28 @@ namespace MNB_SOAP
 
         public void RefreshData()
         {
+            Rates.Clear();
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+
             var xml = new XmlDocument();
             xml.LoadXml(result);
 
             foreach (XmlElement element in xml.DocumentElement)
             {
-                var rate = new RateData();
+                var rate = new RateDate();
                 Rates.Add(rate);
 
                 rate.Date = DateTime.Parse(element.GetAttribute("date"));
@@ -65,6 +81,21 @@ namespace MNB_SOAP
                 chartArea.AxisY.MajorGrid.Enabled = false;
                 chartArea.AxisY.IsStartedFromZero = false;
             }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
 
     }
