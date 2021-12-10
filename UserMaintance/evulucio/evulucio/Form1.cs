0fa402a79cbe3afb714c2ebd2c8035aa60219cfd
Viktor Maric <viktor.maric@stud.uni-corvinus.hu>
2021-12-10 13:18:41 +0000

    iteráció hozzáadása

--- UserMaintance/evulucio/evulucio/Form1.cs
+++ UserMaintance/evulucio/evulucio/Form1.cs
@@ -24,9 +24,29 @@ namespace evulucio
             InitializeComponent();
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+            gc.GameOver += Gc_GameOver;
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
